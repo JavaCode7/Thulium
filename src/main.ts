b26d24d6f521522252f066ad51c86073641d2e2a
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-01 12:01:58 +0000

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+const code: string = "101010100000101010101010000101110000000010000110";
+
+function init(): void {
+    // Create the thulium cpu
+    var cells: Array<any> = Array(1000);
+    var spec: Array<bigint> = Array(5);
+}
