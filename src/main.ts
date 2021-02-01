29faf9a389433b9ada001e39c5f4264d53031f73
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-01 13:06:49 +0000

    Commands are outputted in list

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,44 @@
-const code: string = "101010100000101010101010000101110000000010000110";
+import { readFileSync } from 'fs';
 
-function init(): void {
-    // Create the thulium cpu
-    var cells: Array<any> = Array(1000);
-    var spec: Array<bigint> = Array(5);
+const code = readFileSync(`./` + process.argv[2], 'utf-8');
+
+function _2DArray(): Array<any> {
+    let arr = new Array(1000);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(1000);
+        for (let j = 0; j< arr[i].length; j++) {
+            arr[i][j] = 0;
+        }
+    }
+    return arr;
 }
+
+function init(): Array<any> {
+    // Create the thulium cpu
+    let cells: Array<any> = _2DArray()
+    let spec: Array<number> = Array(5);
+    let memory: number = 0;
+    let spec_index: number = 0;
+    let index: number = 0;
+
+    return [cells, spec, memory, spec_index, index]
+}
+
+function thulium(data: Array<any>): void{
+    var thu = {
+        cells: data[0],
+        spec: data[1],
+        memory: data[2],
+        spec_index: data[3],
+        index: data[4],
+        code: code
+    }
+    let commands: Array<string> = thu.code.split("\r\n");
+    console.log(commands);
+}
+
+function main(): void {
+    thulium(init())
+}
+
+main()
