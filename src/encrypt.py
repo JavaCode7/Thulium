9e57fede139c37576f88b510dc82f6adb4998aa6
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-02 21:48:57 +0000

    Create encrypt.py

---
+++ src/encrypt.py
@@ -0,0 +1,21 @@
+from os import strerror as strerr
+import sys
+
+bfr = open(sys.argv[1], 'r')
+ctn: list = [i for i in bfr.read()]
+contents: list = []
+for i in ctn:
+    contents.append(int(i, 16))
+print(contents)
+
+data = bytearray(len(contents))
+
+for i in range(len(data)):
+    data[i] = contents[i]
+try:
+    bfw = open(sys.argv[1], 'wb')
+    bfw.write(data)
+    bfw.close()
+    bfr.close()
+except IOError as e:
+    print("I/O error occurred:", strerr(e.errno))
