70c7c0961d22053c0ea3ea98c2e7360e171098e0
JavaCode7 <76915005+JavaCode7@users.noreply.github.com>
2021-02-04 09:34:54 +0000

    Finish Thulium 2

--- src/decrypt.py
+++ src/decrypt.py
@@ -1,3 +1,14 @@
 import sys
 bfr = open(sys.argv[1], "rb")
+data: list = list(bfr.read())
+bin_data: list = data[:int(len(data)/2) - 1]
+num_data: list = data[int(len(data)/2):]
+to_bin: list = []
+del num_data[0]
+for i in range(len(bin_data)):
+    a: str = format( bin_data[i] - int ( chr ( num_data[i] ) , 36 ), "b")
+    to_bin.append(str ("0" * (5 - len(a)) + a))
 bfw = open(sys.argv[1], "w")
+bfw.write('\n'.join(to_bin))
+bfr.close()
+bfw.close()
--- src/encrypt.py
+++ src/encrypt.py
@@ -1,6 +1,7 @@
 from os import strerror as strerr
 import random
 import sys
+import numpy
 
 bfr = open(sys.argv[1], 'r')
 ctn: list = [i for i in bfr.read()]
@@ -12,18 +13,18 @@ for i in ctn:
 data = bytearray(len(contents))
 
 for d in range(len(contents)):
-    d = random.randint(0, 32)
+    d = numpy.base_repr(random.randint(0, 35), 36)
     r.append(d)
 
 for i in range(len(data)):
-    data[i] = contents[i] + r[i]
+    data[i] = contents[i] + int(str(r[i]), 36)
 try:
     bfw = open(sys.argv[1], 'wb')
     bfw2 = open(sys.argv[1], 'a')
     bfw.write(data)
     bfw2.write("\n")
     for i in r:
-        bfw2.write(str(i) + " ")
+        bfw2.write(str(i))
     bfw.close()
     bfr.close()
     bfw2.close()
